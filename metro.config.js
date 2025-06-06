d693dfb114b78b6e333972e82cee344930f9eda8
Vinay Negi <vinegi004@gmail.com>
2025-06-06 17:14:54 +0530

    Publish eas build

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
+//const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
